efbbdaaa30cb9465544547e25f90dbe17693c2ab
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-06-08 16:44:17 +0200

    Project setup : structure files & folders

---
+++ app/constants/index.ts
@@ -0,0 +1,8 @@
+export const processSteps = [
+  { id: "postcode", label: "Postcode", path: "/postcode" },
+  { id: "waste-type", label: "Waste Type", path: "/waste-type" },
+  { id: "select-skip", label: "Select Skip", path: "/select-skip" }, // Your target page
+  { id: "permit-check", label: "Permit Check", path: "/permit-check" },
+  { id: "choose-date", label: "Choose Date", path: "/choose-date" },
+  { id: "payment", label: "Payment", path: "/payment" },
+];
---
+++ app/index.d.ts
@@ -0,0 +1,8 @@
+declare interface CardProps {
+  title: string;
+  description: string;
+}
+
+declare interface CardListProps {
+  cards: CardProps[];
+}
