b096f85b2e54624bb311d7d0a4785e3370807537
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-06-08 22:14:19 +0200

    UI : build the skiOptions component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,17 +15,20 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       <Header />
 
-      <div className="w-full max-w-xl text-center py-2 px-4 md:py-4">
+      <div className="w-full text-center py-2 px-4 md:py-4">
         <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
           Choose Your Skip Size
         </h1>
-        <p className="mt-1 text-md leading-8 text-gray-600">
+        <p className="mt-1 w-full  text-md leading-8 text-gray-600">
           Select the skip size that best suits your needs.Not Sure ? See our
-          size guide for help comparing options.
+          size guide .
         </p>
       </div>
 
       <SkipSizeGuide />
+      <div className="w-full flex justify-center my-4 items-start">
+        <h1 className="text-start text-xl font-bold">Compare Skip Options</h1>
+      </div>
       <SkipOptions />
     </div>
   );
--- components/skipSection/skipSizeGuide.tsx
+++ components/skipSection/skipSizeGuide.tsx
@@ -3,8 +3,8 @@ import { categoryData } from "~/constants";
 const SkipSizeGuide = () => {
   return (
     <section className="w-full bg-white">
-      <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+      <div className="max-w-8xl mx-4 py-2 px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4">
           {categoryData.map((category) => (
             <div
               key={category.name}
@@ -36,7 +36,7 @@ const SkipSizeGuide = () => {
                 </p>
 
                 {/* Description */}
-                <p className="mt-2 text-md font-light text-gray-600">
+                <p className="mt-2 text-sm font-light text-gray-600">
                   {category.description}
                 </p>
               </div>
